81be7759ebe802073c38234fa79727faf759f815
Gajam-Anurag <89657709+Gajam-Anurag@users.noreply.github.com>
2022-04-05 12:48:52 -0700

    05 APRIL

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,20 @@
 const Home = () => {
-    return ( 
 
+    const Home = (e) => {
+        console.log("Hello",e);
+
+    }
+    const Home2 = (name,e) => {
+
+        console.log("Hello " + name, e.target);
+
+    }
+    return ( 
+   
         <div className="home">
             <h2>My Home blog</h2>
+            <button onClick = {Home} > Click Me</button>
+            <button onClick = { (e) => Home2('Anurag Gajam',e) }>Click for Name</button>
         </div>
      );
 }
