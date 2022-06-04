c12b8fb7aa619bc4bd2013228a901ff2bb58f4bd
Anurag Gajam <37669266+AnuragGajam@users.noreply.github.com>
2022-06-04 09:08:46 +0530

    tut 17

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = ({blogs, title, handleDelete}) => {
+const BlogList = ({blogs, title}) => {
     // const blogs = props.blogs;
     // const title = props.title;
     return (  
@@ -7,9 +7,7 @@ const BlogList = ({blogs, title, handleDelete}) => {
             {blogs.map((blog) => (
                 <div className="blog-pre" key={blog.id}>
                     <h2>{blog.title}</h2>
-                    <h3>{blog.body}</h3>
                     <p>written by {blog.author}</p>  
-                    <button onClick = {() => handleDelete(blog.id)}>Delete Blog</button>
                 </div>
             ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -4,31 +4,27 @@ import BlogList from './BlogList';
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState( [
-        { title: 'My new website', body: 'Chaala unnay lopala daachinam anthe', author: 'Anurag', id: 1 },
-        { title: 'Welcome party!', body: 'Pynadhi chooshaka nka loothuga choopistha', author: 'Gajam', id: 2 },
-        { title: 'Web dev top tips', body: 'Avvanni chooshaka neek oopika undadhu le', author: 'Anurag Gajam', id: 3 }]
-    );
-    const handleDelete = (id) => {
+    const [blogs, setBlogs] = useState(null)
 
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-
-    }
 
     const [name, setName] = useState("Anurag Gajam")
 
     useEffect( () => {
-        console.log("useEffect renders when we change data based on useState");
-        console.log(name);
-    }, [name]);
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setBlogs(data)
+        })
+    }, []);
 
     return ( 
    
         <div className="home">
-            <BlogList blogs = {blogs} title = "My blogs" handleDelete = {handleDelete}/>
-            <BlogList blogs = {blogs.filter((blog) => blog.author == 'Gajam')}  title = "Gajam's Blogs" handleDelete = {handleDelete}/>
-           {/* <button onClick={() => setName("Anusha")}>Change Name</button>
+           { blogs && <BlogList blogs = {blogs} title = "My blogs" />}
+           {/* <BlogList blogs = {blogs.filter((blog) => blog.author == 'Gajam')}  title = "Gajam's Blogs" handleDelete = {handleDelete}/>
+            <button onClick={() => setName("Anusha")}>Change Name</button>
             <p>{name}</p>*/}
         </div>
      );
