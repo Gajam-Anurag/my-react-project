09ea2ec49fd7b4942dd37829c063b342efb52834
Anurag Gajam <37669266+AnuragGajam@users.noreply.github.com>
2022-06-05 09:28:32 +0530

    video 20

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,15 @@
 import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
-
+import useFetch from './useFetch';
 
 const Home = () => {
-
-    const [blogs, setBlogs] = useState(null)
-
-
-    const [name, setName] = useState("Anurag Gajam")
-
-    useEffect( () => {
-        fetch('http://localhost:8000/blogs')
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
-            setBlogs(data)
-        })
-    }, []);
-
+    const {data: blogs, isPending, isError} = useFetch('http://localhost:8000/blogs')
+    
     return ( 
    
         <div className="home">
+           {isError && <div>{isError}</div>}
+           {isPending && <div>Loading ....</div>}
            { blogs && <BlogList blogs = {blogs} title = "My blogs" />}
            {/* <BlogList blogs = {blogs.filter((blog) => blog.author == 'Gajam')}  title = "Gajam's Blogs" handleDelete = {handleDelete}/>
             <button onClick={() => setName("Anusha")}>Change Name</button>
---
+++ src/useFetch.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+
+    const [data, setdata] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [isError, setError] = useState(null);
+
+    useEffect( () => {
+        setTimeout(() => {
+            fetch(url)
+        .then(res => {
+            console.log(res)
+            if (!res.ok){
+                throw Error('Could not fetch the data for that resource')
+            }
+            return res.json();
+        })
+        .then(data => {
+            setdata(data)
+            setIsPending(false)
+            setError(null)
+        })
+        .catch( err => {
+            setIsPending(false);
+            setError(err.message);
+            })
+        }, 1000);
+    }, [url]);
+    return {data, isPending, isError}
+}
+
+export default useFetch;
