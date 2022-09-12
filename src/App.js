55b2d3f031078fe4d7aef8f248d7113d5ac0c73d
Gajam-Anurag <89657709+Gajam-Anurag@users.noreply.github.com>
2022-09-12 10:41:44 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Navbar from './Navbar';
 import Home from './Home';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
 import NotFound from './NotFound';
